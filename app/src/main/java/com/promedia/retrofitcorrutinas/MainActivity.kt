16644340379cb19b578d6d01a56cabd660fe967b
KotlinPromedia <kotlin@gmx.es>
2022-07-19 18:15:56 +0200

    Añado comentarios

--- app/src/main/java/com/promedia/retrofitcorrutinas/MainActivity.kt
+++ app/src/main/java/com/promedia/retrofitcorrutinas/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.promedia.retrofitcorrutinas
 
-import android.app.Activity
+/*
+*   Proyecto extraido de https://cursokotlin.com
+*   URL: https://cursokotlin.com/tutorial-retrofit-2-en-kotlin-con-corrutinas-consumiendo-api-capitulo-20-v2/
+*   YotubeURL: https://www.youtube.com/watch?v=aQP-mUGWh1U
+*   Autor: AristiDevs
+ */
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
@@ -14,21 +20,29 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+// Al heredar de SearchView.OnQueryTextListener debemos sobrescribir 2 métodos
+// onQueryTextSubmit() y onQueryTextChange()
 class MainActivity : AppCompatActivity(),SearchView.OnQueryTextListener {
     private lateinit var b : ActivityMainBinding
     private lateinit var adapter : DogAdapter
+    // Necesito crear una variable para inicializar el Adapter
+    // Ya que el adapter debe inicializarse cuando se carga la app
+    // pero aún no tenemos las imágenes
     private val dogImages = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
+        // Activo el listener del SearchView
         b.searchView.setOnQueryTextListener(this)
         initRecyclerView()
     }
 
     private fun initRecyclerView() {
+        // Inicializo el adapter con una lista vacía
         adapter = DogAdapter(dogImages)
+        // Inicializo el RecyclerView
         b.rvDogs.layoutManager = LinearLayoutManager (this)
         b.rvDogs.adapter = adapter
     }
@@ -65,11 +79,13 @@ class MainActivity : AppCompatActivity(),SearchView.OnQueryTextListener {
                 } else {
                     showError() // Función propia que muestra el error
                 }
+                // Función propia que oculta el teclado cuando pulso la lupa
                 hideKeyBoard()
             }
         }
     }
-    // Función que oculta el teclado
+
+    // Función que oculta el teclado cuando pulso la lupa
     private fun hideKeyBoard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(b.constraintLayoutMain.windowToken, 0)
